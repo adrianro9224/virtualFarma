9a2bd0e07fa06744406fabe0861570a4ed566387
adrian-home <adrian.romero9224@gmail.com>
2015-03-26 23:25:41 -0500

    amend search widget

--- application/views/admin/seller/templates/sales-creator.php
+++ application/views/admin/seller/templates/sales-creator.php
@@ -3,7 +3,7 @@
 		<label class="sr-only" for="searchProductInput">Search</label>
 		<div class="input-group">
 			<div class="input-group-addon"></div>
-			<input type="text" class="form-control" ng-model="search" id="searchProductInput" placeholder="Producto a buscar">
+			<input type="text" class="form-control" ng-change="search( searchText )" ng-model="searchText" id="searchProductInput" placeholder="Producto a buscar">
 			<div class="input-group-addon"></div>
 		</div>
 	</div>
@@ -12,22 +12,11 @@
 			<h3>Resultados</h3>
 		</div>
 		<div class="panel-body">
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th>Nombre</th>
-					<th>Presentación</th>
-					<th>Precio</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr ng-repeat="product in products | filter:search">
-					<td class="name" ng-bind="product.name"></td>
-					<td ng-bind="product.presentation"></td>
-					<td ng-bind="product.price"></td>
-				</tr>
-			</tbody>
-		</table>					
+			<a ng-repeat="product in results">
+				<span class="name" ng-bind="product.name"></span>
+				<span ng-bind="product.presentation"></span>
+				<span ng-bind="product.price"></span>
+			</a>
 		</div>
 	</div>
 </div>
--- public/app/controllers/SalesCreatorCtrl.js
+++ public/app/controllers/SalesCreatorCtrl.js
@@ -2,22 +2,42 @@
  * Created by adrian on 25/03/15.
  */
 
-farmapp.controller('SalesCreatorCtrl', ['$scope', '$http', function( $scope, $http ){
+farmapp.controller('SalesCreatorCtrl', ['$scope', '$http', '$filter', function( $scope, $http, $filter ){
+
+    $scope.noResults = false;
 
     function load_products() {
-        
+        var json;
         $http.get("http://virtualfarma.com.co/admin/all_products")
             .success(function (data, status, headers, config) {
-                json = angular.fromJson(data);
-                $scope.products = json;
+                var json = angular.fromJson(data);
+
+                if ( json != undefined )
+                    $scope.products = json;
+
             }).
             error(function (data, status, headers, config) {
                 console.info(data + ":(");
             });
 
+
+
     }
 
     load_products();
+
+    $scope.search = function( searchText ) {
+
+        if ( searchText.length > 2){
+            var result = $filter('filter')( $scope.products, searchText, undefined)
+
+            if ( result.length > 0 )
+                $scope.results = result;
+            else
+                $scope.noResults = true;
+        }
+
+    }
 
 
 }]);
