c61347fa652fb0221743dc80348444795243a0ea
adrian-home <adrian.romero9224@gmail.com>
2015-09-16 22:39:33 -0500

    add new slides

--- public/app/controllers/CarouselCtrl.js
+++ public/app/controllers/CarouselCtrl.js
@@ -16,12 +16,12 @@ farmapp.controller('CarouselCtrl', ['$scope', function($scope) {
         slides.push({
             image: 'http://virtualfarma.com.co/assets/images/slides/' + i + format,
             button: ['','http://virtualfarma.com.co/product/search_product/argel','http://virtualfarma.com.co/product/search_product/promelight'][slides.length % 4],
-            class : ['hidden', 'argel-button', 'promelight-button'][slides.length % 4]
+            class : ['hidden', 'entrenador-vaginal-button', 'argel-button', 'promelight-button'][slides.length % 4]
         });
     };
     for (var i=0; i<=3; i++) {
 
-        if ( i != 2 )
+        //if ( i != 2 )
             $scope.addSlide(i);
 
     }
